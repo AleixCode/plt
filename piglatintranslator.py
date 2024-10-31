f2c47a18093a02d6429d27bb26260c1f6c6d9dcb
Aleix L <aleixcode@gmail.com>
2024-10-31 15:36:17 +0000

    user story 1

---
+++ piglatintranslator.py
@@ -0,0 +1,15 @@
+from piglatin import PigLatin
+
+class PigLatinTranslator:
+    def __init__(self):
+        self.translator = None
+
+    def PigLatinTranslator(self, phrase: str):
+        self.translator = PigLatin(phrase)
+        return self.translator
+
+    def get_phrase(self) -> str:
+        return self.translator.get_phrase()
+
+    def translate(self) -> str:
+        return self.translator.translate()
--- test/test_piglatin.py
+++ test/test_piglatin.py
@@ -1,9 +1,17 @@
 import unittest
 from piglatin import PigLatin
+from piglatintranslator import PigLatinTranslator
 from error import PigLatinError
 
 
 class TestPigLatin(unittest.TestCase):
 
-    def test_something(self):
-        pass
+    def setUp(self):
+        self.PigLatinTranslator = PigLatinTranslator()
+
+
+    def test_get_phrase(self):
+        self.PigLatinTranslator.PigLatinTranslator("hello world")
+        self.assertEqual("hello world", self.PigLatinTranslator.get_phrase())
+
+
